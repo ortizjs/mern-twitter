d8fd6363fca6b3272b39a655d744a02d34b23a45
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-27 12:52:27 -0800

    Add configuration to app and users file as well as models

--- app.js
+++ app.js
@@ -5,19 +5,29 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
 .connect(db, { useNewUrlParser: true })
 .then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+
+// app.get("/", (req, res) => {
+//     // console.log(res);
+//     // debugger;
+//     res.send("Hello World!");
+//   });
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 
 const port = process.env.PORT || 5000;
