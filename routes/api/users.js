f37472333d7076afb2cb7370889e94847d62f070
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-27 13:49:47 -0800

    Add validation for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,12 +5,19 @@ const User = require('../../models/User');
 const keys = require('../../config/keys');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => {
     res.json({ msg: "This is the users route" });
 });
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
     // Check to make sure nobody has already registered with a duplicate email
     User.findOne({ email: req.body.email })
         .then(user => {
