c5fb06746855526664f413e79433c317c57189aa
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-28 11:15:12 -0800

    Add additional tweets routes

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -8,6 +8,28 @@ router.get("/test", (req, res) => {
     res.json({ msg: "This is the tweets route" });
 });
 
+router.get("/", (req, res) => {
+    Tweet
+        .find()
+        .sort({ date: -1 })
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(400).json(err));
+});
+
+router.get("/user/:user_id", (req, res) => {
+    Tweet
+        .find({ user: req.params.user_id })
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(400).json(err));
+});
+
+router.get("/:id", (req, res) => {
+    Tweet
+        .findById(req.params.id)
+        .then(tweet => res.json(tweet))
+        .catch(err => res.status(400).json(err));
+});
+
 router.post('/',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
